e81dbb1d43d2970eaa04d6995c48b8a25c44072a
JoshKing56 <joshuaiking56@gmail.com>
2018-11-27 20:01:28 +0000

    Forgot to actually add the files lol

---
+++ pySerial test/UDP_Client.py
@@ -0,0 +1,29 @@
+import socket
+import sys
+import serial
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ('localhost', 10000)
+ser = serial.Serial('/dev/ttyACM0', 230400)
+
+def readPins():
+    while 1:
+        print(ser.readline().decode("utf-8").strip("\n"))  # decode because serial monitor returns byte stream
+
+def sendToServer():
+   while 1:
+        try:
+            # Send data
+            pinnumber = ser.readline()
+            print('sending "%s"' % pinnumber.decode('utf-8').strip('\n'))
+            sent = sock.sendto(pinnumber, server_address)
+
+            # Receive response
+            data, server = sock.recvfrom(4096)
+
+        finally:
+            print('closing socket (not actually)')
+
+sendToServer()
--- pySerial test/serialtest.py
+++ pySerial test/serialtest.py
@@ -2,7 +2,7 @@ import serial
 
 
 def readPins():
-    ser = serial.Serial('/dev/ttyACM0', 57600)
+    ser = serial.Serial('/dev/ttyACM0', 230400)
     while 1:
         print(ser.readline().decode("utf-8").strip("\n"))  # decode because serial monitor returns byte stream
 
