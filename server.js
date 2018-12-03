911511e4534b226ad47c85c1a303451759f5274e
hugo1005 <hugojdolan@gmail.com>
2018-12-03 12:24:17 +0000

    Animation stuff added

--- server.js
+++ server.js
@@ -8,16 +8,26 @@ app.use(bodyParser.json());
 
 var current_data = {"src": {'a': 1, 'b':1}, "ref": {'a':'Bluetooth', 'b':'Bluetooth'}}
 
-let rawdata = fs.readFileSync('./items.json');  
-let item_translation = JSON.parse(rawdata);
+let item_data = fs.readFileSync('./items.json');  
+let item_translation = JSON.parse(item_data);
+
+let anim_data = fs.readFileSync('./anims.json');  
+let anims_matrix = JSON.parse(anim_data);
 
 // Post data format: {a: INT, b:INT}
 app.post('/update', (req, res, next)=>{
     current_data.src = req.body;
     
+    anim_data_item = anims_matrix[`${current_data.src.a}+${current_data.src.b}`] 
+    
+    if(anim_data_item != undefined){
+        current_data.src['c'] = anim_data_item.src
+    }
+    
     current_data.ref = {
         'a': item_translation[current_data.src.a], 
-        'b': item_translation[current_data.src.b] 
+        'b': item_translation[current_data.src.b],
+        'c': anim_data_item.msg
     }
 
     io.emit('new_match', current_data, {for: 'everyone'});
